3f27895959590704fe351902b46be8261701564b
Tomasz Stanisz <tomasz.stanisz@orange.com>
2024-10-18 19:26:50 +0100

    Fix test environment

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
 	preset: '@testing-library/react-native',
 	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
 	setupFilesAfterEnv: ['./jest-setup.ts'],
+	transformIgnorePatterns: ['node_modules/(?!@toolz/allow)/" --env=jsdom'],
 	testMatch: ['**/*.test.{ts,tsx}']
 }
